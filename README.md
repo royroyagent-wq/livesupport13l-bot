88b7b9a2b82bfc42728e23de3a30b44ce6c78da7
royroyagent-wq <royroyagent@gmail.com>
2026-05-17 14:27:29 +0530

    Update and rename README.md to config.py

--- README.md
+++
@@ -1,1 +0,0 @@
-# livesupport13l-bot
---
+++ config.py
@@ -0,0 +1,9 @@
+# livesupport13l-botBOT_TOKEN = "8963748019:AAHnJJFSOKDjK1tGFSa4ONvzknLqQUEGb5Q"
+
+ADMIN_ID = 7848668334
+
+REGISTER_LINK = "https://13lwin13.com/register?inviteCode=W99AFHN&from=web"
+
+PREDICTION_CHANNEL = "https://t.me/Ritik_number_sureshot"
+
+UID_RULE = "6"
